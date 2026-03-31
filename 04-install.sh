93763d07a143afe14c346361cd631c2815a7d963
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-03-31 21:35:36 +0530

    Installations of packages

---
+++ 04-install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USER-ID=$(id -u)
+
+if [ $USER-ID -eq 0 ]; then
+    echo "you have root  privilages for installing packages"
+else
+    echo "ERROR:: you don't have root privilages for instalinng packages"
+    exit 1
+fi
+
+VALIDATE(){
+
+    if [$1 -eq 0 ]; then
+    echo "Installation of $2 is successful"
+else
+    echo "Installation of $2 is failed"
+    exit 1
+fi
+
+}
+
+dnf install mysql -y
+VALIDATE $? "MySql"
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+
+
+
+
+

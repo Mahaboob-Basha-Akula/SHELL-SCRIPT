c1f3009361a0a1032c321d425bdd605116b8f2d3
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2026-04-02 22:06:10 +0530

    loops added

--- 08-script.sh
+++ 08-script.sh
@@ -21,22 +21,32 @@ VALIDATE(){
     fi
 }
 
+for softwares in $@
+do
+        dnf list installed mysql
+    if [ $? -ne 0 ]; then
+        dnf install $softwares -y
+        VALIDATE $? "$softwares"
+    else
+        echo -e "$softwares already exits...$Y SKIPPING $N"
+    fi
+done
 
 
-dnf list installed mysql
-if [ $? -ne 0 ]; then
-    dnf install mysql -y
-    VALIDATE $? "MYSQL"
-else
-    echo -e "Package already exits...$Y SKIPPING $N"
-fi
+# dnf list installed mysql
+# if [ $? -ne 0 ]; then
+#     dnf install mysql -y
+#     VALIDATE $? "MYSQL"
+# else
+#     echo -e "Package already exits...$Y SKIPPING $N"
+# fi
 
-dnf list installed nginx
-if [ $? -ne 0 ]; then 
-    dnf install nginx -y
-    VALIDATE $? "NGINX"
-else
-    echo -e "Package already exits...$Y SKIPPING $N"
-fi
+# dnf list installed nginx
+# if [ $? -ne 0 ]; then 
+#     dnf install nginx -y
+#     VALIDATE $? "NGINX"
+# else
+#     echo -e "Package already exits...$Y SKIPPING $N"
+# fi
 
 
